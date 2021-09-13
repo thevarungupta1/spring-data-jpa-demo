8a4c30832aa0c79220863f4f1a03d7aba6dd17f4
Varun Gupta <thevarungupta1@gmail.com>
2021-09-14 00:19:28 +0530

    JPA Native @Query annotation

--- src/main/java/com/thevarungupta/spring/data/jpa/demo/repository/StudentRepository.java
+++ src/main/java/com/thevarungupta/spring/data/jpa/demo/repository/StudentRepository.java
@@ -2,8 +2,12 @@ package com.thevarungupta.spring.data.jpa.demo.repository;
 
 import com.thevarungupta.spring.data.jpa.demo.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -18,5 +22,37 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     public List<Student> findByGuardianName(String name);
 
     Student findByFirstNameAndLastName(String firstName, String lastName);
+
+    // JPQL
+    @Query("select s from Student s where s.emailId= ?1")
+    Student getStudentByEmailAddress(String email);
+
+    @Query("select s.firstName from Student s where s.emailId=?1")
+    String getStudentFirstNameByEmailAddress(String emailId);
+
+    // Native
+    @Query(
+            value = "SELECT * from tbl_student s where s.email_address = ?1",
+            nativeQuery = true
+    )
+    Student getStudentByEmailAddressNative(String emailId);
+
+    // native named params
+    @Query(
+            value = "SELECT * from tbl_student s where s.email_address = :emailId",
+            nativeQuery = true
+    )
+    Student getStudentByEmailAddressNativeNamedParams(
+            @Param("emailId") String emailId
+    );
+
+    @Modifying
+    @Transactional
+    @Query(
+            value = "update tbl_student set first_name = ?1 where email_address = ?2",
+            nativeQuery = true
+    )
+    int updateStudentNameByEmailId(String firstName, String emailId);
+
 
 }
--- src/test/java/com/thevarungupta/spring/data/jpa/demo/SpringDataJpaDemoApplicationTests.java
+++ src/test/java/com/thevarungupta/spring/data/jpa/demo/SpringDataJpaDemoApplicationTests.java
@@ -13,62 +13,94 @@ import java.util.List;
 @SpringBootTest
 class SpringDataJpaDemoApplicationTests {
 
-	@Autowired
-	private StudentRepository studentRepository;
+    @Autowired
+    private StudentRepository studentRepository;
 
-	@Test
-	public void saveStudent(){
-		Student student = Student.builder()
-				.firstName("Mark")
-				.lastName("Smith")
-				.emailId("m@gmail.com")
+    @Test
+    public void saveStudent() {
+        Student student = Student.builder()
+                .firstName("Mark")
+                .lastName("Smith")
+                .emailId("m@gmail.com")
 //				.guardianName("Paul")
 //				.guardianEmail("p@gmail.com")
 //				.guardianMobile("99999")
-				.build();
-		studentRepository.save(student);
-	}
+                .build();
+        studentRepository.save(student);
+    }
 
-	@Test
-	public void saveStudentWithGuardian(){
-		Guardian guardian = Guardian.builder()
-				.name("Some name")
-				.email("s@gmail.com")
-				.mobile("77777")
-				.build();
+    @Test
+    public void saveStudentWithGuardian() {
+        Guardian guardian = Guardian.builder()
+                .name("Some name")
+                .email("s@gmail.com")
+                .mobile("77777")
+                .build();
 
-		Student student = Student.builder()
-				.firstName("Stacy")
-				.lastName("Watson")
-				.emailId("s@gmail.com")
-				.guardian(guardian)
-				.build();
+        Student student = Student.builder()
+                .firstName("Stacy")
+                .lastName("Watson")
+                .emailId("s@gmail.com")
+                .guardian(guardian)
+                .build();
 
-		studentRepository.save(student);
-	}
+        studentRepository.save(student);
+    }
 
-	@Test
-	public void displayAllStudents(){
-		List<Student> studentList = studentRepository.findAll();
-		System.out.println(studentList);
-	}
+    @Test
+    public void displayAllStudents() {
+        List<Student> studentList = studentRepository.findAll();
+        System.out.println(studentList);
+    }
 
-	@Test
-	public void displayStudentByFirstName(){
-		List<Student> studentList = studentRepository.findByFirstName("Mark");
-		System.out.println(studentList);
-	}
+    @Test
+    public void displayStudentByFirstName() {
+        List<Student> studentList = studentRepository.findByFirstName("Mark");
+        System.out.println(studentList);
+    }
 
-	@Test
-	public void displayStudentByFirstNameContaining(){
-		List<Student> studentList = studentRepository.findByFirstNameContaining("Ma");
-		System.out.println(studentList);
-	}
+    @Test
+    public void displayStudentByFirstNameContaining() {
+        List<Student> studentList = studentRepository.findByFirstNameContaining("Ma");
+        System.out.println(studentList);
+    }
 
-	@Test
-	public void displayStudentBasedOnGuardianName(){
-		List<Student> studentList = studentRepository.findByGuardianName("Paul");
-		System.out.println(studentList);
-	}
+    @Test
+    public void displayStudentBasedOnGuardianName() {
+        List<Student> studentList = studentRepository.findByGuardianName("Paul");
+        System.out.println(studentList);
+    }
+
+    @Test
+    public void displayGetStudentByEmailAddress() {
+        Student student = studentRepository.getStudentByEmailAddress("m@gmail.com");
+        System.out.println(student);
+    }
+
+    @Test
+    public void displayGetStudentNameByEmailAddress() {
+        String name = studentRepository.getStudentFirstNameByEmailAddress("s@gmail.com");
+        System.out.println(name);
+    }
+
+    @Test
+    public void displayGetStudentByEmailAddressNative() {
+        Student student = studentRepository.getStudentByEmailAddressNative("s@gmail.com");
+        System.out.println(student);
+    }
+
+    @Test
+    public void displayGetStudentByEmailAddressNativeNamedParams() {
+        Student student = studentRepository.getStudentByEmailAddressNativeNamedParams("s@gmail.com");
+        System.out.println(student);
+    }
+
+    @Test
+    public void TestFor_updateStudentNameByEmailAddress(){
+        studentRepository.updateStudentNameByEmailId(
+                "Mark200",
+                "m@gmail.com"
+        );
+    }
 
 }
